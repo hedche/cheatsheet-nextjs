905a5f2027572a27743e781a1e8045646c20e270
Hedche <montyrhodes.apple@gmail.com>
2024-06-14 15:23:43 +0100

    Adding initial changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DevOps Cheatsheet</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/hedche/cheatsheet-nextjs',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'If you like this project, please give it a star!',
   },
 }
 
